7254e94765b84ed03a27a0c6e559e7a21037dfd7
TheBeornegar <47784919+TheBeornegar@users.noreply.github.com>
2019-07-25 05:57:05 +0200

    Update WinLostForm.cs

--- SomewhatClicker/WinLostForm.cs
+++ SomewhatClicker/WinLostForm.cs
@@ -13,7 +13,6 @@ namespace SomewhatClicker
 {
     public partial class WinLostForm : Form
     {
-
         Form3 _f3;
         Form2 _f2;
 
@@ -31,8 +30,6 @@ namespace SomewhatClicker
                 throw new ArgumentNullException("f2");
             }
             _f2 = f2;
-
-
         }
 
         private void WinLostForm_Load(object sender, EventArgs e)
@@ -45,14 +42,12 @@ namespace SomewhatClicker
                 label1.Text = "You lost.";
                 BackgroundImage= Properties.Resources.NO;
                 button1.Text = "Exit";
-
             }
             else
             {
                 label1.Text = "You won.";
                 BackgroundImage = Properties.Resources.OK;
                 button1.Text = "Continue";
-
             }
         }
 
@@ -80,7 +75,5 @@ namespace SomewhatClicker
                 file.WriteLine(Atributes.CriticalChance.ToString());
             }
         }
-
-        
     }
 }
